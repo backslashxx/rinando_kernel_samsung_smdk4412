b1f7a90b7d38b2f5fe02d13af07f75038b0864b7
David Howells <dhowells@redhat.com>
2014-09-16 17:36:01 +0100

    UPSTREAM: Provide a binary to hex conversion function
    Provide a function to convert a buffer of binary data into an unterminated
    ascii hex string representation of that data.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Acked-by: Vivek Goyal <vgoyal@redhat.com>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -410,6 +410,7 @@ static inline char *pack_hex_byte(char *buf, u8 byte)
 
 extern int hex_to_bin(char ch);
 extern int __must_check hex2bin(u8 *dst, const char *src, size_t count);
+extern char *bin2hex(char *dst, const void *src, size_t count);
 
 /*
  * General tracing related utility functions - trace_printk(),
