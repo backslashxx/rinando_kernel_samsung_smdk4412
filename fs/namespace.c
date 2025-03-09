ca6fc84a845bcba0a0fe835ae06f3a916cb6ad17
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-03-10 07:12:13 +0800

    HACK: namespace: can_umount: adapt to 3.4
    this is not available on 3.10

--- fs/namespace.c
+++ fs/namespace.c
@@ -1218,13 +1218,9 @@ static int can_umount(const struct path *path, int flags)
 {
 	struct mount *mnt = real_mount(path->mnt);
 
-	if (!may_mount())
-		return -EPERM;
 	if (path->dentry != path->mnt->mnt_root)
 		return -EINVAL;
 	if (!check_mnt(mnt))
-		return -EINVAL;
-	if (mnt->mnt.mnt_flags & MNT_LOCKED) /* Check optimistically */
 		return -EINVAL;
 	if (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))
 		return -EPERM;
