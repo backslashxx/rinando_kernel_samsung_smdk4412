d23ffeee10d7358e9986b7aea8017ea20c62da26
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-03-10 17:18:28 +0800

    KernelSU: disable session keyring workarounds for old kernels
    3.4 is too old...

--- drivers/kernelsu/core_hook.c
+++ drivers/kernelsu/core_hook.c
@@ -589,7 +589,7 @@ int ksu_handle_setuid(struct cred *new, const struct cred *old)
 }
 
 // kernel 4.9 and older
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) || defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)
+#if 0
 int ksu_key_permission(key_ref_t key_ref, const struct cred *cred,
 			      unsigned perm)
 {
@@ -630,7 +630,7 @@ static struct security_hook_list ksu_hooks[] = {
 	LSM_HOOK_INIT(task_prctl, ksu_task_prctl),
 	LSM_HOOK_INIT(inode_rename, ksu_inode_rename),
 	LSM_HOOK_INIT(task_fix_setuid, ksu_task_fix_setuid),
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) || defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)
+#if 0
 	LSM_HOOK_INIT(key_permission, ksu_key_permission)
 #endif
 };
