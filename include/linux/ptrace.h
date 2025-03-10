a0753c97be8509d41d20e1a5de5ca1a0c74166d0
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-03-10 17:31:58 +0800

    ARM: provide current_user_stack_pointer
    cherry picked from:
    
    `new helper: current_user_stack_pointer()`
    - https://github.com/torvalds/linux/commit/1ca97bb541a1f5a735e697a8bba763cde3aab452
    
    `ARM: 7374/1: add TRACEHOOK support`
    https://github.com/torvalds/linux/commit/0693bf68148c4473158e435383e75af70b704f78
    
    `arm: optimized current_pt_regs()`
    https://github.com/torvalds/linux/commit/bfd170d56505bf21cb636b0f1f169eaa815bdfe4

--- include/linux/ptrace.h
+++ include/linux/ptrace.h
@@ -366,6 +366,10 @@ static inline void user_single_step_siginfo(struct task_struct *tsk,
 #define arch_ptrace_stop(code, info)		do { } while (0)
 #endif
 
+#ifndef current_user_stack_pointer
+#define current_user_stack_pointer() user_stack_pointer(current_pt_regs())
+#endif
+
 extern int task_current_syscall(struct task_struct *target, long *callno,
 				unsigned long args[6], unsigned int maxargs,
 				unsigned long *sp, unsigned long *pc);
