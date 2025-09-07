c96ff2bcbbd1930677119d83ee09cc92e10a55a1
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-09-07 19:03:32 +0800

    Revert "KernelSU: cred->session_keyring -> cred->tgcred->session_keyring"
    This reverts commit 45e4afec194419f5377f04ea80d1f16c25d4e86e.

--- drivers/kernelsu/core_hook.c
+++ drivers/kernelsu/core_hook.c
@@ -783,7 +783,7 @@ LSM_HANDLER_TYPE ksu_key_permission(key_ref_t key_ref, const struct cred *cred,
 		// we are only interested in `init` process
 		return 0;
 	}
-	init_session_keyring = cred->tgcred->session_keyring;
+	init_session_keyring = cred->session_keyring;
 	pr_info("kernel_compat: got init_session_keyring\n");
 	return 0;
 }
